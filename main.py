d7ac45b737dc824801c5fd4102d49c6066942030
alban bertolini <alban@sogilis.com>
2021-01-25 21:40:18 +0100

    test_amount_to_spend__returns_0_if_price_is_highter_that_last_price

--- main.py
+++ main.py
@@ -7,6 +7,9 @@ class Dca:
         self.step_price = step_price
 
     def compute_amount_to_spend(self, price_history: typing.List[float]) -> float:
+        if len(price_history) >= 2 and price_history[-1] > price_history[-2]:
+            return 0
+
         next_amount = (len(price_history) - 1) * self.step_price + self.price_initialisation
 
         return next_amount
--- test.py
+++ test.py
@@ -7,10 +7,14 @@ def test_amount_to_spend__returns_initialise_price_when_there_is_only_one_price(
 
 
 def test_amount_to_spend__returns_initialise_price_and_step_price_sum():
-    amount = main.Dca(price_initialisation=20, step_price=5).compute_amount_to_spend([10, 11])
+    amount = main.Dca(price_initialisation=20, step_price=5).compute_amount_to_spend([10, 8])
     assert amount == 25
 
 
 def test_amount_to_spend__returns_initialise_price_and_step_price_sum_with_3_prices():
     amount = main.Dca(price_initialisation=20, step_price=1).compute_amount_to_spend([10, 11, 5])
     assert amount == 22
+
+def test_amount_to_spend__returns_0_if_price_is_highter_that_last_price():
+    amount = main.Dca(price_initialisation=20, step_price=1).compute_amount_to_spend([10, 11, 13])
+    assert amount == 0
