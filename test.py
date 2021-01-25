c0d174ff76afcf382775279d2ed7f3a698b00c16
alban bertolini <alban@sogilis.com>
2021-01-25 21:37:23 +0100

    test_amount_to_spend__returns_initialise_price_and_step_price_sum_with_3_prices

--- test.py
+++ test.py
@@ -9,3 +9,8 @@ def test_amount_to_spend__returns_initialise_price_when_there_is_only_one_price(
 def test_amount_to_spend__returns_initialise_price_and_step_price_sum():
     amount = main.Dca(price_initialisation=20, step_price=5).compute_amount_to_spend([10, 11])
     assert amount == 25
+
+
+def test_amount_to_spend__returns_initialise_price_and_step_price_sum_with_3_prices():
+    amount = main.Dca(price_initialisation=20, step_price=1).compute_amount_to_spend([10, 11, 5])
+    assert amount == 22
