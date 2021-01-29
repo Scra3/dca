38d0dfdc04cbcac9c05e36294381600e2260009c
alban bertolini <alban@sogilis.com>
2021-01-29 10:30:45 +0100

    add dca configuration in json file

--- config.py
+++ config.py
@@ -1,2 +1,4 @@
 TEST_ENV = "test"
 ENV = "ENV"
+DCA_CONFIGURATION_FILE = "dca_configuration.json"
+
--- main.py
+++ main.py
@@ -2,11 +2,14 @@ import dca_runner as runner
 import dca as dca
 import price_history as price_history
 import portfolio as portfolio
+import config
 
 if __name__ == '__main__':
+    with open(config.DCA_CONFIGURATION_FILE) as json_file:
+        config = dca.DcaConfiguration.serialise(json_file.read())
+
     dca_runner = runner.DcaRunner(price_history=price_history.PriceHistory(),
-                                  dca=dca.Dca(dca.DcaConfiguration(price_initialisation=20, step_price=1,
-                                                                   max_total_amount_to_spend=2000)))
+                                  dca=dca.Dca(config=config))
     dca_runner.run()
 
     print(f"balance: {portfolio.Portfolio().get_balance()}")
