b8e1b79fbd6e2f96279477ee20845c839adc9263
alban bertolini <alban@sogilis.com>
2021-01-29 21:48:54 +0100

    improve naming

--- api/kraken_api.py
+++ api/kraken_api.py
@@ -8,16 +8,18 @@ class PairMapping(enum.Enum):
     ETHEUR = "XETHZEUR"
 
 
+WAITING_BEFORE_RETRY_SECOND = 10
+
+
 class KrakenApi:
     @staticmethod
     def get_current_pair_price(pair: str) -> float:
-        parameters = {
-            "pair": pair
-        }
+        parameters = {"pair": pair}
         response = requests.get("https://api.kraken.com/0/public/Ticker", params=parameters)
+
         if response.ok:
             return float(response.json()["result"][PairMapping[pair].value]["a"][0])
         else:
-            time.sleep(10)
+            time.sleep(WAITING_BEFORE_RETRY_SECOND)
             print("[retry] Kraken API is done")
             return KrakenApi.get_current_pair_price(pair)
