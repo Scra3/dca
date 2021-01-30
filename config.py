c9a6aa32eaf6d6e1ac2d5e9d00f0264a308ce68a
alban bertolini <alban@sogilis.com>
2021-01-30 20:38:49 +0100

    add configuration

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TEST_ENV = "test"
 PRODUCTION_ENV = "production"
 ENV = "ENV"
-DCA_CONFIGURATION_FILE = "dca_configuration.json"
+DCA_CONFIGURATION_FILE = "dca_configuration_production.json"
 
--- main.py
+++ main.py
@@ -9,20 +9,25 @@ import time
 
 def job():
     with open(config.DCA_CONFIGURATION_FILE) as json_file:
-        dca_configuration = model.DcaConfiguration.serialise(json_file.read())
+        dca_configuration: model.DcaConfiguration = model.DcaConfiguration.serialise(json_file.read())
     dca_runner = runner.DcaRunner(broker=model.Broker(),
                                   dca_configuration=dca_configuration)
     dca_runner.run()
     print(f"balance: {model.Portfolio().get_balance()}")
     print(f"amounts spent: {model.Portfolio().get_amounts_spent()}")
     print(f"total: {model.Portfolio().get_total_spent()}")
-    print(f"prices: {model.PriceHistory().get_prices()}")
     print(f"average: {model.Portfolio().get_average_price()}")
+    print(f"prices: {model.price_history.PriceHistory().get_prices()}")
+
+    print(f"----------------MAIN INFO----------------------")
+    print(f"Unrealized gain: {model.Portfolio().get_unrealized_gains()}")
+    print(f"Unrealized percentage gain: {model.Portfolio().get_unrealized_gains_in_percentage()}%")
+    print(f"Broker balance: {model.Broker().get_balance(dca_configuration.traded_pair)}")
+    print(f"-----------------------------------------------")
 
 
 if __name__ == '__main__':
-    job()
-    # schedule.every(1).minutes.do(job)
-    # while True:
-    #   schedule.run_pending()
-    #  time.sleep(1)
+    schedule.every(1).minutes.do(job)
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
