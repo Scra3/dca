2f4d5213a374514abe84b5b529ae4837b56d1cf3
alban bertolini <alban@sogilis.com>
2021-04-10 20:19:04 +0200

    remove db before running test

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -4,7 +4,9 @@ import model
 
 @pytest.fixture
 def drop_databases_after_test():
-    yield
     model.Order().drop_db()
     model.PriceHistory().drop_db()
     model.Log().drop_db()
+
+    yield
+
