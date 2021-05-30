b4f7aafed7a2055a86d48746f3853a111fa1ba49
alban bertolini <alban@sogilis.com>
2021-05-30 20:45:51 +0200

    fix typing

--- api/telegram_api.py
+++ api/telegram_api.py
@@ -9,8 +9,9 @@ TELEGRAM_KEY_FILE = "telegram.key"
 class TelegramApi:
     @staticmethod
     def send_message(message: str):
-        if TelegramApi.available_key():
-            chat_id, secret = TelegramApi.load_key()
+        file = TelegramApi.get_telegram_key_path()
+        if os.path.isfile(file):
+            chat_id, secret = TelegramApi.load_key(file)
             requests.get(
                 f"https://api.telegram.org/bot{secret}/sendMessage?chat_id={chat_id}&text={message}"
             )
@@ -21,12 +22,8 @@ class TelegramApi:
         return f"{dir_path}/../{TELEGRAM_KEY_FILE}"
 
     @staticmethod
-    def available_key():
-        return os.path.isfile(TelegramApi.get_telegram_key_path())
-
-    @staticmethod
-    def load_key() -> typing.Optional[typing.Tuple[str, str]]:
-        with open(TelegramApi.get_telegram_key_path(), "r") as f:
+    def load_key(path: str) -> typing.Tuple[str, str]:
+        with open(path, "r") as f:
             chat_id = f.readline().strip()
             secret = f.readline().strip()
         return chat_id, secret
